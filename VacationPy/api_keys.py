db4815dee320c16707f2053890e896adca05a4cf
Jorge <georgo5573@gmail.com>
2022-03-22 18:23:30 -0500

    Final push

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "d7354070725520e3a4db0ff23341edbc"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDRUC1A2qwZRufuU_u-5_FhVq_3_T3fwnk"
